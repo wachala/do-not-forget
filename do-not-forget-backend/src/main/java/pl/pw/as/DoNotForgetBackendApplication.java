e7074bfffbc42c451560a33ba092950df7e5287d
arkadiusz <socha.arkadiusz@gmail.com>
2017-04-03 21:07:40 +0200

    Added application skeleton

---
+++ do-not-forget-backend/src/main/java/pl/pw/as/DoNotForgetBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.pw.as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoNotForgetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoNotForgetBackendApplication.class, args);
+	}
+}
