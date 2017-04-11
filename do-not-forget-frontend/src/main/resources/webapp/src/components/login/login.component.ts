2934431f87577ed409f7d60d9973f537df42cd4c
arkadiusz <socha.arkadiusz@gmail.com>
2017-04-11 22:55:26 +0200

    Added login service

--- do-not-forget-frontend/src/main/resources/webapp/src/components/login/login.component.ts
+++ do-not-forget-frontend/src/main/resources/webapp/src/components/login/login.component.ts
@@ -1,14 +1,23 @@
 import {Component} from "@angular/core";
 import {URL_COMPONENT_BASE} from "../../url.constants";
 import {LoginData} from "../../model/LoginData";
+import {LoginService} from "../../services/LoginService";
 @Component({
     selector: 'login-view',
+    providers: [LoginService],
     templateUrl: URL_COMPONENT_BASE + 'login/login.component.html'
 })
 export class LoginComponent {
-    loginData:LoginData = new LoginData;
+    constructor(private _loginService: LoginService) {
+
+    }
+
+    loginData: LoginData = new LoginData;
 
     login() {
-        console.log(this.loginData);
+        this._loginService.loginUser(this.loginData)
+            .subscribe(success => {
+                this.loginData = new LoginData
+            });
     }
 }
