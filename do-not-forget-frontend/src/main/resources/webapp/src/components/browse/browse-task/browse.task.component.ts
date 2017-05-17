4f4f23ea148cecdae8b7fa2395e337c46fe5d13e
arkadiusz <socha.arkadiusz@gmail.com>
2017-05-17 17:14:11 +0200

    Added retrieving recently expired tasks in frontend

--- do-not-forget-frontend/src/main/resources/webapp/src/components/browse/browse-task/browse.task.component.ts
+++ do-not-forget-frontend/src/main/resources/webapp/src/components/browse/browse-task/browse.task.component.ts
@@ -7,18 +7,21 @@ import {AlertService} from "../../../services/AlertService";
 import {AlertConfig} from "../../../model/alert/AlertConfig";
 import {ErrorService} from "../../../services/ErrorService";
 import {Router} from "@angular/router";
-import {DateValidationUtils} from "../../../utils/date.validator.utils";
 import {TaskUtils} from "../../../utils/task.utils";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {UserService} from "../../../services/UserService";
 
 @Component({
     selector: 'browse-tasks',
-    providers: [TaskService, AlertService, ErrorService],
+    providers: [TaskService, AlertService, ErrorService, NgbModal, UserService],
     templateUrl: URL_COMPONENT_BASE + 'browse/browse-task/browse.task.component.html'
 //
 })
 export class BrowseTasksComponent {
-    tasks:Array<Task>;
+    tasks: Array<Task>;
+    recentlyExpiredTasks: Array<Task>;
 
+    expiredTaskAmount:number = 0;
     alertConfig: AlertConfig = AlertConfig.getAlertToClose();
     newTaskState = TaskState.NEW;
     inProgressTaskState = TaskState.IN_PROGRESS;
@@ -27,18 +30,35 @@ export class BrowseTasksComponent {
     historicalTaskFilterString = "";
 
     constructor(private _taskService: TaskService, private _alertService: AlertService,
-                private _errorService: ErrorService, private _router: Router) {
+                private _errorService: ErrorService, private _router: Router, private _modalService: NgbModal,
+                private _userService: UserService) {
 
     }
 
     ngOnInit(): void {
         this._loadTasks();
+        this._loadRecentlyExpiredTasks()
     }
 
     private _loadTasks() {
         this._taskService.getAllTasks().subscribe(
             (data) => {
                 this.tasks = data;
+            },
+            (error) => {
+                let errorMsg = this._errorService.handleExceptionAndReturnMessage(error);
+                this.alertConfig = this._alertService.retrieveErrorAlertShowConfig(errorMsg);
+            }
+        );
+    }
+
+    private _loadRecentlyExpiredTasks() {
+        this._taskService.getRecentlyExpiredTasks().subscribe(
+            (data) => {
+                this.recentlyExpiredTasks = data;
+                this._userService.updateLastBrowseTaskDate().subscribe();
+                this.expiredTaskAmount = this.recentlyExpiredTasks.length;
+
             },
             (error) => {
                 let errorMsg = this._errorService.handleExceptionAndReturnMessage(error);
@@ -93,10 +113,16 @@ export class BrowseTasksComponent {
     }
 
     currentTasksPresent() {
-        return this.getCurrentTasks().length!=0;
+        return this.getCurrentTasks().length != 0;
     }
 
     historicalTasksPresent() {
-        return this.getHistoricalTasks().length!=0;
+        return this.getHistoricalTasks().length != 0;
+    }
+
+    openModal(content) {
+        this._modalService.open(content).result.then((result) => {
+            this._loadTasks()
+        });
     }
 }
