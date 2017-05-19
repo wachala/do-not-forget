9f65fd5236b44a8932761655c586924a8253b52c
arkadiusz <socha.arkadiusz@gmail.com>
2017-05-19 10:31:28 +0200

    added some custom styles

--- do-not-forget-frontend/src/main/resources/webapp/src/providers/priority.provider.ts
+++ do-not-forget-frontend/src/main/resources/webapp/src/providers/priority.provider.ts
@@ -1,9 +1,15 @@
 export class PriorityProvider {
     public retrievePriorityOptions() {
-        return ['Very low', 'Low', 'Medium', 'High', 'Urgent'];
+        return [
+            {name: 'Very low', cssclass: 'dnf-very-low-priority'},
+            {name: 'Low', cssclass: 'dnf-low-priority'},
+            {name: 'Medium', cssclass: 'dnf-medium-priority'},
+            {name: 'High', cssclass: 'dnf-high-priority'},
+            {name: 'Urgent', cssclass: 'dnf-urgent-priority'}
+        ];
     }
 
-    public retrieveDefaultPriorityIndex () {
+    public retrieveDefaultPriorityIndex() {
         return 2;
     }
 }
